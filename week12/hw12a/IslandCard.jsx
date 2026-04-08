16e7560358a31cb56b20ad623f4d598c10a43c70
Lilith Gannone <gannone@hawaii.edu>
2026-04-08 13:36:39 -1000

    Update IslandCard.jsx

--- week12/hw12a/IslandCard.jsx
+++ week12/hw12a/IslandCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-//manually altered, AI assisted for the intitial structuring of the component. I needed assistance understanding what was required for the ({name, description, tip}) => { ... } syntax. Styling manually coded.
+//manually altered, AI-assisted for the initial structuring of the component. I needed assistance understanding what was required for the ({name, description, tip}) => { ... } syntax. Styling manually coded.
 // receives island data from App.jsx and displays it in a card format. ID not needed for this component as it doesn't need to be displayed for users. 
 function IslandCard({ name, description, tip }) {
     const customStyle = {
