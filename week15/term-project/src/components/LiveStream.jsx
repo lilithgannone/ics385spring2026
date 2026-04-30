55541184fa9d3a2d67d2a124af35496753ae08ad
Lilith Gannone <gannone@hawaii.edu>
2026-04-30 09:22:18 -1000

    Update LiveStream.jsx

--- week15/term-project/src/components/LiveStream.jsx
+++ week15/term-project/src/components/LiveStream.jsx
@@ -6,11 +6,12 @@ function LiveStream() {
       <h3>Kilauea Live Stream: Get Ready to Experience Kilauea</h3>
       <div className="stream-frame" aria-hidden="true">
         <iframe
-          src="https://www.youtube.com/embed/HggWKlZv9yk?autoplay=1&mute=1&controls=0&disablekb=1&modestbranding=1&playsinline=1&loop=1&playlist=HggWKlZv9yk"
+          src="https://www.youtube.com/embed/HggWKlZv9yk?si=U3aIr3Dvk-oS6Eeq"
           title="Kilauea Live Stream"
-          allow="autoplay; encrypted-media; picture-in-picture"
-          allowFullScreen
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
         ></iframe>
       </div>
     </article>
