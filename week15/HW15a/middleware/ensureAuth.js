c6f65884285155e622a66edaab4afcd5ff2657be
Lilith Gannone <gannone@hawaii.edu>
2026-04-30 09:03:50 -1000

    Delete week15/HW15a directory

--- week15/HW15a/middleware/ensureAuth.js
+++
@@ -1,5 +0,0 @@
-module.exports = function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    
-    return res.redirect('/');
-};
--- week15/HW15a/term-project/middleware/isAuthenticated.js
+++
@@ -1,7 +0,0 @@
-module.exports = function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated() && req.user && req.user.role === "admin") {
-    return next();
-  }
-
-  res.redirect("/admin/login");
-};
